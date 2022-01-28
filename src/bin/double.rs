9cd1d8d8248a9693cd59fcef941f64a4ca72ae44
muyuu <anticyborg@gmail.com>
2022-01-28 23:25:23 +0900

    impl double with panic

--- src/bin/double.rs
+++ src/bin/double.rs
@@ -1,1 +1,18 @@
-fn main() {}
+fn get_int_from_file() -> i32 {
+    let path = "number.txt";
+
+    // ファイルがない場合はパニックを起こす
+    let num_str = std::fs::read_to_string(path).expect("failed to open the file.");
+
+    // ファイルにかかれている文字列を数値にパースできない場合はパニックを起こす
+    let ret = num_str
+        .trim()
+        .parse::<i32>()
+        .expect("failed to parse string to a number.");
+
+    ret * 2
+}
+
+fn main() {
+    println!("{}", get_int_from_file());
+}
