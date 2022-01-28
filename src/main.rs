0054f5c4f44fe97764ed51b099d10207f33b1fbc
muyuu <anticyborg@gmail.com>
2022-01-28 22:19:00 +0900

    add struct RpnCalculator
    fn eval を実装するがまだ実計算はしていない

--- src/main.rs
+++ src/main.rs
@@ -42,3 +42,15 @@ fn run<R: BufRead>(reader:R, verbose: bool) {
         println!("{}", line);
     }
 }
+
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        0
+    }
+}
