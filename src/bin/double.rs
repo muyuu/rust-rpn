c64a00803ffa2536ff069fe830a5752da026e1a4
muyuu <anticyborg@gmail.com>
2022-01-28 23:40:50 +0900

    use Result

--- src/bin/double.rs
+++ src/bin/double.rs
@@ -1,18 +1,23 @@
-fn get_int_from_file() -> i32 {
+fn get_int_from_file() -> Result<i32, String> {
     let path = "number.txt";
 
-    // ファイルがない場合はパニックを起こす
-    let num_str = std::fs::read_to_string(path).expect("failed to open the file.");
+    // ファイルがない場合はエラーオブジェクトを文字列にする
+    // 最後の ? は Result 型を返す演算子
+    // 直前の結果の Result 型の値が Ok(t) なら t を返し Err(e) なら Err(e) で早期リターン
+    let num_str = std::fs::read_to_string(path).map_err(|e| e.to_string())?;
 
-    // ファイルにかかれている文字列を数値にパースできない場合はパニックを起こす
-    let ret = num_str
+    num_str
         .trim()
         .parse::<i32>()
-        .expect("failed to parse string to a number.");
-
-    ret * 2
+        // parse() の結果が Ok の場合は2倍して Ok(t * 2) となる
+        .map(|t| t * 2)
+        // parse() の結果が Err の場合は e の文字列を返して Err(e) となる
+        .map_err(|e| e.to_string())
 }
 
 fn main() {
-    println!("{}", get_int_from_file());
+    match get_int_from_file() {
+        Ok(x) => println!("{}", x),
+        Err(e) => println!("{}", e),
+    }
 }
