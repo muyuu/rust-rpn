f8b3600b2ba90ce706ba9281e20e0e4e69148a30
muyuu <anticyborg@gmail.com>
2022-01-28 21:43:32 +0900

    initial commit
    clap を使って逆ポーランド記法の計算ツールを作る
    本コミットは clap でファイルと verbose オプションを受け付けるだけ

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("My RPN program")
+        .version("1.0.0")
+        .author("muyuu")
+        .about("Super awesome sample RPN calculator")
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .required(false),
+        )
+        .arg(
+            Arg::new("formula_file")
+                .value_name("FILE")
+                .index(1)
+                .required(false),
+        )
+        .get_matches();
+
+    match matches.value_of("formula_file") {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified"),
+    }
+
+    let verbose = matches.is_present("verbose");
+    println!("Is verbosity specified?: {}", verbose);
+}
